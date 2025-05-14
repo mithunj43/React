33f5f7e5faee3ca74f5e03ef46b01149f1963ce3
mithunj43 <mithunj43@gmail.com>
2025-05-14 08:04:04 +0530

    CSS Styling and CSS modules

--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -1,10 +1,10 @@
-
+import classes from './Post.module.css'
 function Post(props) {
     
     return (
-        <div style={{ color: 'red', textAlign: 'left'}}>
-            <p>{props.author}</p>
-            <p>{props.body}</p>
+        <div className={classes.post}>
+            <p className={classes.author}>{props.author}</p>
+            <p className={classes.text}>{props.body}</p>
         </div>
     );
 }
