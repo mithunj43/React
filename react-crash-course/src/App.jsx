15c7b005b83c83ddd3be16d630374dcd70b9d8b9
mithunj43 <mithunj43@gmail.com>
2025-05-14 19:06:42 +0530

    Exercise & Another Component

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Post from './components/post'
+import PostsList from './components/PostsList'
 
 function App() {
 
   return (
   <>
-    <Post author ="Mithun" body ="React.js is awesome"/>
-    <Post author ="Pooja" body ="Check out the full course"/>
+  <PostsList />
   </>
   ); 
 }
