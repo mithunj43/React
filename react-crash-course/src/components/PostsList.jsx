59f4cdea684b454720ea6a4b39494c366361a2cb
mithunj43 <mithunj43@gmail.com>
2025-05-15 07:30:38 +0530

    Lifting state Up

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,13 +1,23 @@
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
+import { useState } from 'react';
 
 function PostsList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    function changeBodyHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+    function changeAuthorHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
    return (
     <>
-        <NewPost />
+        <NewPost onBodyChange = {changeBodyHandler} onAuthorChange = {changeAuthorHandler}/>
         <ul className={classes.posts}>
-        <Post author ="Mithun" body ="React.js is awesome"/>
+        <Post author = {enteredAuthor} body = {enteredBody}/>
         <Post author ="Pooja" body ="Check out the full course"/>
         </ul>
     </>
