a7c1532c01a6c3e03b67076b4a7f8a0e226d5285
mithunj43 <mithunj43@gmail.com>
2025-05-16 07:43:31 +0530

    State and Conditional Content

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -14,11 +14,18 @@ function PostsList() {
     function changeAuthorHandler(event) {
         setEnteredAuthor(event.target.value);
     }
+
+    const [modelIsVisible, setModelIsVisible] = useState(true);
+    function hideModelHandler() {
+        setModelIsVisible(false);
+    }
    return (
     <>
-        <Modal>
+        {modelIsVisible ? (
+        <Modal onClose = {hideModelHandler}>
             <NewPost onBodyChange = {changeBodyHandler} onAuthorChange = {changeAuthorHandler}/>
-        </Modal>
+        </Modal> ) : null}
+        
         <ul className={classes.posts}>
         <Post author = {enteredAuthor} body = {enteredBody}/>
         <Post author ="Pooja" body ="Check out the full course"/>
